d581ef0958bdb7ae1eba5ae401067f120cc5b5e7
mousse <hpmousse@gmail.com>
2020-02-25 17:19:09 +0100

    commit122

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 use OAuth_io\OAuth;
 
 $oauth = new OAuth();
@@ -8,16 +8,16 @@ $oauth->initialize('S3HRKiyOpoKAyCHNB8RCZqRoV1w', 'qKVuYvpguf2nC_HdHsGK4');
 
 // Exemple with Google (using ZendFramwork, Controller /oauth)
 // Action /signin (url: /oauth/authorize)
-public function signinAction() {
+signinAction(provider) {
     try {
-        $this->oauth->redirect('spotify', 'index.php');
+        $this->oauth->redirect(provider, 'index.php');
     } catch (\Exception $e) {
         echo $e->getMessage();
     }
 }
 
 // Action /redirect (url: /oauth/redirect)
-public function redirectAction() {
+redirectAction() {
     try {
         $request_object = $this->oauth->auth('spotify', array(
             'redirect' => true
@@ -29,13 +29,16 @@ public function redirectAction() {
     //You can use $request_object to make API calls on behalf of your user
 }
 
-$request_object = $oauth->auth('spotify', array(
-    'credentials' => $credentials,
-    'force_refresh' => true
-));
+if($request_object){
+	$request_object = $oauth->auth('spotify', array(
+    	'credentials' => $credentials,
+    	'force_refresh' => true
+	));
+	$result = $request_object->me(array('firstname', 'lastname', 'email'));
+	dump($result);
+}else{
+	signinAction('spotify');
+}
 
-$result = $request_object->me(array('firstname', 'lastname', 'email'));
-
-dump($result);
 
 ?>
