4c14951b4c0fb06a6ed17cd99b7418179d12655c
mousse <hpmousse@gmail.com>
2020-02-25 17:31:28 +0100

    commit122

--- index.php
+++ index.php
@@ -6,28 +6,8 @@ use OAuth_io\OAuth;
 $oauth = new OAuth();
 $oauth->initialize('S3HRKiyOpoKAyCHNB8RCZqRoV1w', 'qKVuYvpguf2nC_HdHsGK4');
 
-// Exemple with Google (using ZendFramwork, Controller /oauth)
-// Action /signin (url: /oauth/authorize)
-function signinAction(provider) {
-    try {
-        $this->oauth->redirect(provider, 'index.php');
-    } catch (\Exception $e) {
-        echo $e->getMessage();
-    }
-}
 
-// Action /redirect (url: /oauth/redirect)
-function redirectAction(provider) {
-    try {
-        $request_object = $this->oauth->auth(provider, array(
-            'redirect' => true
-        ));
-    } catch (\Exception $e) {
-        die($e->getMessage());
-    }
-    //Your user is authorized by Google
-    //You can use $request_object to make API calls on behalf of your user
-}
+$oauth->redirect('spotify', 'index.php');
 
 if($request_object){
 	$request_object = $oauth->auth('spotify', array(
